b78cb9e40bb3da68da8af5180579e9c01fe11302
pawansiwakoti <pawanshiwakoti13gmail.com>
2020-06-07 01:48:14 +1000

    updated the tipdescription with color for tipdescription

--- app/src/main/java/com/pk/android/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/pk/android/tipcalculator/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.pk.android.tipcalculator
 
+import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.SeekBar
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
+
 
 private const val INITIAL_TIP_PERCENT = 15
 class MainActivity : AppCompatActivity() {
@@ -16,9 +18,11 @@ class MainActivity : AppCompatActivity() {
 
         seekBar.progress = INITIAL_TIP_PERCENT
         txtpercent.text = "$INITIAL_TIP_PERCENT%"
+        TipDiscription(INITIAL_TIP_PERCENT)
         seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar:  SeekBar?, progress: Int, fromUser: Boolean) {
                 txtpercent.text ="$progress"
+                TipDiscription(progress)
                 calculateTip()
             }
 
@@ -39,8 +43,32 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    private fun TipDiscription(progress: Int) {
+        val tipDiscription : String
+        val tipPercent = seekBar.progress
+        when (tipPercent){
+            in 0..9 -> tipDiscription = "Poor"
+            in 10..14 -> tipDiscription ="Acceptable"
+            in 15..19 -> tipDiscription = "Good"
+            in 20..24 -> tipDiscription = "Great"
+            else -> tipDiscription = "Amazing"
+        }
+        txtTipDescription.text = tipDiscription.toString()
+        val color = ArgbEvaluator().evaluate(
+            tipPercent.toFloat()/seekBar.max,
+            ContextCompat.getColor(this, R.color.colorWorstTip),
+                ContextCompat.getColor(this, R.color.colorBestTip)
+        ) as Int
+        txtTipDescription.setTextColor(color)
+    }
+
     private fun calculateTip() {
         //Get the value of the base and tip percent
+        if (edTxtBase.text.toString().isEmpty()){
+            txtTipAmount.text =""
+            txtAmount.text = ""
+            return
+        }
         val baseAmount = edTxtBase.text.toString().toDouble()
         val tipPercent = seekBar.progress
         val tipAmount = baseAmount * tipPercent / 100
